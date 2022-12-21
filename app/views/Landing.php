e4783d612186ad767d75e089a9598395eedb204a
aymane-smi <aymanebel2@outlook.fr>
2022-12-21 09:46:51 +0100

    working onthe front-end

--- app/views/Landing.php
+++ app/views/Landing.php
@@ -1,52 +1,120 @@
 <!DOCTYPE html>
-    <head>
-        <title>ElectroMaroc</title>
-        <link rel="stylesheet" href="http://localhost:9000/public/src/css/output.css"/>
-        <link rel="stylesheet" href="http:///localhost:9000/public/src/css/all.min.css"/>
-        
-    </head>
-    <body class="bg-[#faeadd]">
-        <div class="p-[40px]">
-            <nav class="flex justify-between items-center">
-                <p class="font-[500]  text-[20px]"><span class="text-[#87ff54]">e</span>lectro<span class="font-[600]"><span class="text-[#87ff54]">M</span>aroc</span>
-                </p>
-                <div class="flex justify-center items-center gap-[30px] text-[#5C5C5C]">
+
+<head>
+    <title>ElectroMaroc</title>
+    <link rel="stylesheet" href="http://localhost:9000/public/src/css/output.css" />
+    <link rel="stylesheet" href="http:///localhost:9000/public/src/css/all.min.css" />
+
+</head>
+
+<body class="bg-[#faeadd] font-">
+    <div class="p-[40px]">
+        <nav class="flex justify-between items-center">
+            <p class="font-[500]  text-[20px]"><span class="text-[#87ff54]">e</span>lectro<span class="font-[600]"><span class="text-[#87ff54]">M</span>aroc</span>
+            </p>
+            <div class="flex justify-center items-center gap-[30px] text-[#5C5C5C]">
                 <i class="fa-light fa-user text-[20px]"></i>
                 <i class="fa-light fa-cart-shopping text-[20px]"></i>
                 <i class="fa-light fa-bars text-[20px]"></i>
-                </div>
-            </nav>
-            <div class="mt-10 w-full rounded-[10px] flex flex-col justify-center items-center font-si">
-                <span class="text-[25px] font-semibold">Bienvenu chez ElectroMaroc</span>
-                <span class="text-[20px] font-medium">le 1er distributer des composants éléctroniques dans la royaum</span>
-                <a class="text-white bg-black p-5 mt-10 rounded-tl-[15px] rounded-br-[15px] font-semibold">explore notre produits</a>
+            </div>
+        </nav>
+        <div class="mt-10 w-full rounded-[10px] flex flex-col justify-center items-center font-si">
+            <span class="text-[25px] font-semibold">Bienvenu chez ElectroMaroc</span>
+            <span class="text-[20px] font-medium">le 1er distributer des composants éléctroniques dans la royaum</span>
+            <a class="text-white bg-black p-5 mt-10 rounded-tl-[15px] rounded-br-[15px] font-semibold">explore notre produits</a>
+        </div>
+    </div>
+    <div class="bg-black w-screen p-8">
+        <p class="text-white text-[20px] font-bold">Choissiais votre catégorie
+        <p>
+        <p class="text-white text-[17px] font-medium mt-5">vous trouvrez tous les catégories qui concerne le gaming, électro-ménagé et autres...</p>
+        <div class="flex flex-wrap justify-center items-center gap-[50px] mt-10">
+            <div class="text-white w-[250px]">
+                <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+                <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
+            </div>
+            <div class="text-white w-[250px]">
+                <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+                <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
+            </div>
+            <div class="text-white w-[250px]">
+                <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+                <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
+            </div>
+            <div class="text-white w-[250px]">
+                <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+                <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
             </div>
         </div>
-        <div class="bg-black w-screen p-8">
-            <p class="text-white text-[20px] font-bold">Choissiais votre catégorie<p>
-            <p class="text-white text-[17px] font-medium mt-5">vous trouvrez tous les catégories qui concerne le gaming, électro-ménagé et autres...</p>
-            <div class="flex flex-wrap justify-start items-center gap-[50px] mt-10">
-                <div class="text-white w-[250px]">
-                    <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default"/>
-                    <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
+    </div>
+    <div class="p-[40px] bg-white w-full">
+        <p class="text-[23px] font-bold pb-7 border-b-[1px] border-gray-400 mb-10">Notre Produit</p>
+        <div class="flex flex-wrap justify-center items-center mb-10 gap-20">
+            <div>
+                <div class="relative overflow-hidden rounded-md w-fit border-[1px] border-[#5C5C5C]">
+                    <img class="w-[200px] h-[200px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+                    <i class="fa-light fa-cart-shopping text-[20px] text-red-500 absolute top-1 right-1"></i>
                 </div>
-                <div class="text-white w-[250px]">
-                    <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default"/>
-                    <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
+                <p class="mt-3 flex justify-between items-center">
+                    <span class="font-800 text-[15px] text-gray-500">nom</span>
+                    <span class="font-800 text-[11px] text-gray-500 rounded-sm p-1 bg-gray-200">catégorie</span>
+                </p>
+                <p class="mt-3 font-semibold text-[15px] text-gray-500">titre</p>
+                <p class="mt-3 flex justify-between items-center">
+                    <span class="mt-3 font-semibold text-[18px]">120 MAD</span>
+                    <a class="p-1 text-gray-500 border-[1px] border-gray-200 rounded-md hover:border-black hover:bg-black hover:text-white" href="#">Détaille</a>
+                </p>
+            </div>
+            <div>
+                <div class="relative overflow-hidden rounded-md w-fit border-[1px] border-[#5C5C5C]">
+                    <img class="w-[200px] h-[200px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+                    <i class="fa-light fa-cart-shopping text-[20px] text-red-500 absolute top-1 right-1"></i>
                 </div>
-                <div class="text-white w-[250px]">
-                    <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default"/>
-                    <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
-                </div>
-                <div class="text-white w-[250px]">
-                    <img class="h-[300px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default"/>
-                    <p class="p-4 bg-white text-black font-semibold flex justify-center items-center">catégorie</p>
-                </div>
+                <p class="mt-3 flex justify-between items-center">
+                    <span class="font-800 text-[15px] text-gray-500">nom</span>
+                    <span class="font-800 text-[11px] text-gray-500 rounded-sm p-1 bg-gray-200">catégorie</span>
+                </p>
+                <p class="mt-3 font-semibold text-[15px] text-gray-500">titre</p>
+                <p class="mt-3 flex justify-between items-center">
+                    <span class="mt-3 font-semibold text-[18px]">120 MAD</span>
+                    <a class="p-1 text-gray-500 border-[1px] border-gray-200 rounded-md hover:border-black hover:bg-black hover:text-white" href="#">Détaille</a>
+                </p>
             </div>
         </div>
-        <div class="p-[40px] bg-white">
-            <p class="text-[23px] font-bold pb-7 border-b-[1px] border-gray-400">Notre Produit</p>
-            <a class="p-4 bg-black text-white">Voir tous les produits</a>
+        <a class="p-4 bg-black text-white rounded-tl-[15px] rounded-br-[15px] font-semibold mx-auto my-0 block w-fit">Voir tous les produits</a>
+    </div>
+    <footer class="p-3 w-screen bg-black">
+        <div class=" p-5 flex flex-wrap justify-around items-start border-b-[1px] border-white mb-5 pb-[20px]">
+            <div class="w-[350px]">
+                <p class="font-[500]  text-[25px] text-white"><span class="text-[#87ff54]">e</span>lectro<span class="font-[600]"><span class="text-[#87ff54]">M</span>aroc</span>
+                </p>
+                <p class="text-white font-semibold text-[18px] mt-2">le 1er distributer des composants éléctroniques dans la royaum</p>
+            </div>
+            <div class="w-[350px] ml-8">
+                <p class="font-[500]  text-[25px] text-white">Contact</p>
+                <ul class="list-none text-white font-medium">
+                    <li class="mt-1">
+                        <i class="fa-brands fa-whatsapp"></i>
+                        <span class="ml-2">Whatsapp</span>
+                    </li>
+                    <li class="mt-1">
+                        <i class="fa-brands fa-facebook"></i>
+                        <span class="ml-2">Facebook</span>
+                    </li>
+                    <li class="mt-1">
+                        <i class="fa-light fa-envelope"></i>
+                        <span class="ml-2">test@test.com</span>
+                    </li>
+                    <li class="mt-1">
+                        <i class="fa-light fa-phone"></i>
+                        <span class="ml-2">123456789</span>
+                    </li>
+                </ul>
+            </div>
+            <iframe class="rounded-md w-1/2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53963.1823009469!2d-9.271712304968345!3d32.29307251678047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdac212049843597%3A0x6b618c47dfd85d40!2sSafi!5e0!3m2!1sen!2sma!4v1671570363250!5m2!1sen!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-    </body>
+        <p class="mx-auto my-0 text-white block w-fit">electroMaroc © 2022</p>
+    </footer>
+</body>
+
 </html>
