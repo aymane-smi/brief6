53103574f82178feef7bc46f8fb5a8d355f5f2ee
aymane-smi <aymanebel2@outlook.fr>
2022-12-23 09:14:50 +0100

    working on dashboard

--- app/views/Products.php
+++ app/views/Products.php
@@ -1,6 +1,33 @@
 <?php
-    require_once "./inc/header.php";
+require_once "./inc/header.php";
 ?>
+<div class="p-8 mt-4 bg-white flex gap-20">
+    <div class="w-fit">
+        <p class="text-[21px] font-semibold pb-4 border-b-[2px] border-gray-200 w-full">Catégories</p>
+        <form class="mt-5">
+            <div class="flex flex-row-reverse justify-between items-center">
+                <label for="categorie">Catégorie</label>
+                <input type="checkbox" name="category" id="categorie" />
+            </div>
+        </form>
+    </div>
+    <div class="flex flex-wrap justify-start items-center mb-10 gap-20">
+        <div>
+            <div class="overflow-hidden rounded-md w-fit border-[1px] border-[#5C5C5C]">
+                <img class="w-[200px] h-[200px]" src="http://localhost:9000/public/src/assets/wwdc2019DSC_4114.jpg" alt="default" />
+            </div>
+            <p class="mt-3 flex justify-between items-center">
+                <span class="font-800 text-[15px] text-gray-500">nom</span>
+                <span class="font-800 text-[11px] text-gray-500 rounded-sm p-1 bg-gray-200">catégorie</span>
+            </p>
+            <p class="mt-3 font-semibold text-[15px] text-gray-500">titre</p>
+            <p class="mt-3 flex justify-between items-center">
+                <span class="mt-3 font-semibold text-[18px]">120 MAD</span>
+                <a class="p-1 text-gray-500 border-[1px] border-gray-200 rounded-md hover:border-black hover:bg-black hover:text-white" href="#">Détaille</a>
+            </p>
+        </div>
+    </div>
+</div>
 <?php
-    require_once "./inc/footer.php";
+require_once "./inc/footer.php";
 ?>
--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,36 +1,36 @@
 </div>
 <footer class="p-3 w-screen bg-black">
-        <div class=" p-5 flex flex-wrap justify-around items-start border-b-[1px] border-white mb-5 pb-[20px]">
-            <div class="w-[350px]">
-                <p class="font-[500]  text-[25px] text-white"><span class="text-[#87ff54]">e</span>lectro<span class="font-[600]"><span class="text-[#87ff54]">M</span>aroc</span>
-                </p>
-                <p class="text-white font-semibold text-[18px] mt-2">le 1er distributer des composants éléctroniques dans la royaum</p>
-            </div>
-            <div class="w-[350px] ">
-                <p class="font-[500]  text-[25px] text-white">Contact</p>
-                <ul class="list-none text-white font-medium">
-                    <li class="mt-1">
-                        <i class="fa-brands fa-whatsapp"></i>
-                        <span class="ml-2">Whatsapp</span>
-                    </li>
-                    <li class="mt-1">
-                        <i class="fa-brands fa-facebook"></i>
-                        <span class="ml-2">Facebook</span>
-                    </li>
-                    <li class="mt-1">
-                        <i class="fa-light fa-envelope"></i>
-                        <span class="ml-2">test@test.com</span>
-                    </li>
-                    <li class="mt-1">
-                        <i class="fa-light fa-phone"></i>
-                        <span class="ml-2">123456789</span>
-                    </li>
-                </ul>
-            </div>
-            <iframe class="rounded-md w-[350px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53963.1823009469!2d-9.271712304968345!3d32.29307251678047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdac212049843597%3A0x6b618c47dfd85d40!2sSafi!5e0!3m2!1sen!2sma!4v1671570363250!5m2!1sen!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class=" p-5 flex flex-wrap justify-around items-start border-b-[1px] border-white mb-5 pb-[20px]">
+        <div class="w-[350px]">
+            <p class="font-[500]  text-[25px] text-white"><span class="text-[#87ff54]">e</span>lectro<span class="font-[600]"><span class="text-[#87ff54]">M</span>aroc</span>
+            </p>
+            <p class="text-white font-semibold text-[18px] mt-2">le 1er distributer des composants éléctroniques dans la royaum</p>
         </div>
-        <p class="mx-auto my-0 text-white block w-fit">electroMaroc © 2022</p>
-    </footer>
+        <div class="w-[350px] ">
+            <p class="font-[500]  text-[25px] text-white">Contact</p>
+            <ul class="list-none text-white font-medium">
+                <li class="mt-1">
+                    <i class="fa-brands fa-whatsapp"></i>
+                    <span class="ml-2">Whatsapp</span>
+                </li>
+                <li class="mt-1">
+                    <i class="fa-brands fa-facebook"></i>
+                    <span class="ml-2">Facebook</span>
+                </li>
+                <li class="mt-1">
+                    <i class="fa-light fa-envelope"></i>
+                    <span class="ml-2">test@test.com</span>
+                </li>
+                <li class="mt-1">
+                    <i class="fa-light fa-phone"></i>
+                    <span class="ml-2">123456789</span>
+                </li>
+            </ul>
+        </div>
+        <iframe class="rounded-md w-[350px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53963.1823009469!2d-9.271712304968345!3d32.29307251678047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdac212049843597%3A0x6b618c47dfd85d40!2sSafi!5e0!3m2!1sen!2sma!4v1671570363250!5m2!1sen!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <p class="mx-auto my-0 text-white block w-fit">electroMaroc © 2022</p>
+</footer>
 </body>
 
 </html>
