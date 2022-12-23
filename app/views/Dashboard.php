69d0454da7be348f648431e6cb07045d446166d0
unknown <aymanebel2@outlook.fr>
2022-12-23 17:52:14 +0100

    .

---
+++ app/views/Dashboard.php
@@ -0,0 +1,6 @@
+<?php
+require_once "./inc/dash-header.php";
+?>
+<?php
+require_once "./inc/dash-footer.php";
+?>
---
+++ app/views/inc/dash-header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <title>Dashboard</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="flex justify-start items-start">
+        <nav class="bg-[#3c5fa7] h-screen  w-fit">
+            <div class="flex gap-1 w-fit">
+                <img src="http://localhost:9000/public/src/assets/face.svg" alt="face" class="rounded-full w-[30px] h-[30px]"/>
+                <p class="font-semilBold text-[20px] text-gray-200 flex justify-start items-center">
+                    Hi, <span class="text-white font-medium text-[15px]">username!</span> 
+                </p>
+            </div>
+        </nav>
+        <div class="bg-[#faeadd] max-h-screen overflow-y-scroll w-full">x</div>
