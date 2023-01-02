4c8086fd99615594ef5817d4d7d0022e585584b7
aymane-smi <aymanebel2@outlook.fr>
2023-01-02 15:30:22 +0100

    edit user

---
+++ app/controllers/Account.php
@@ -0,0 +1,23 @@
+<?php
+class Account extends Controller
+{
+    private $User;
+    public function __construct()
+    {
+        $this->User = $this->model("User");
+    }
+
+    public function index()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $this->view("Account");
+        } else {
+            if (isset($_POST["password"])) {
+                $this->User->updateClientWithPwd($_POST["email"], $_POST["username"], $_POST["password"], $_POST["full_name"], $_POST["phone"], $_POST["address"], $_POST["city"], $_POST["id"]);
+            } else {
+                $this->User->updateClientWithoutPwd($_POST["email"], $_POST["username"], $_POST["full_name"], $_POST["phone"], $_POST["address"], $_POST["city"], $_POST["id"]);
+            }
+            header("Location: /");
+        }
+    }
+}
--- app/models/User.php
+++ app/models/User.php
@@ -32,8 +32,40 @@ class User
             return false;
     }
 
+    public function updateClientWithoutPwd($email, $username, $fullName, $phone, $address, $city, $id)
+    {
+        $this->db->query("UPDATE costumer email = :email, username = :username, full_name = :full_name, phone = :phone, address = :address, city = :city WHERE id = :id");
+        $this->db->bind(":email", $email);
+        $this->db->bind(":username", $username);
+        $this->db->bind(":full_name", $fullName);
+        $this->db->bind(":phone", $phone);
+        $this->db->bind(":address", $address);
+        $this->db->bind(":city", $city);
+        $this->db->bind(":id", $id);
+        return $this->db->execute();
+    }
+
+    public function updateClientWithPwd($email, $username, $password, $fullName, $phone, $address, $city, $id)
+    {
+        $this->updateClientWithoutPwd($email, $username, $fullName, $phone, $address, $city, $id);
+        $option = [
+            "cost" => 12,
+        ];
+
+        $password = password_hash($password, PASSWORD_BCRYPT, $option);
+        $this->db->query("UPDATE costumer password = :password WHERE id = :id");
+        $this->db->bind(":password", $password);
+        $this->db->bind(":id", $id);
+        $this->db->execute();
+    }
+
     public function signup($email, $username, $password, $fullName, $phone, $address, $city)
     {
+        $option = [
+            "cost" => 12,
+        ];
+
+        $password = password_hash($password, PASSWORD_BCRYPT, $option);
         $this->db->query("INSERT INTO costumer VALUE(email, username, password, full_name, phone, address, city) VALUES(:email, :username, :password, :full_name, :phone, :address, :city)");
         $this->db->bind(":email", $email);
         $this->db->bind(":username", $username);
